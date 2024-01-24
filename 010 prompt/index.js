a26a80d5f38b7d56f8d0a27df96402bce3048ef1
Rasheed Ahmad <118961857+Rasheed-Ahmad07@users.noreply.github.com>
2024-01-24 22:41:49 +0500

    Add files via upload

---
+++ 010 prompt/index.js
@@ -0,0 +1,23 @@
+// console.log("sadeeq ullah");
+// var name=prompt("wt iz ur name?");
+// var age=prompt("wt iz ur age?");
+// var number=prompt("wt iz ur number?");
+// console.log(name ,age,number );
+// console.log("your age in months is " ,age * 12);
+
+// alert("welcome to js");
+
+// var userresponse = confirm ("r u want to del the item");
+
+// if (userresponse == true){
+//     console.log("item delected");
+// }
+// else{
+//     console.log("item NOT delected");
+
+// };
+var x=2;
+console.warn("sadeeq ullah");
+console.log("bilal");
+console.error("rasheed");
+console.log(x);
